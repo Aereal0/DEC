a6a9b6bb89ff043ea9a289c20b00066569637946
unknown <didacreyescastells@gmail.com>
2025-06-10 21:45:29 +0200

    Carpetas y contenido

---
+++ Python/metrics.py
@@ -0,0 +1,28 @@
+import numpy as np
+from scipy.optimize import linear_sum_assignment
+from sklearn.metrics import normalized_mutual_info_score, adjusted_rand_score
+
+def acc(y_true, y_pred):
+    """
+    Calculate clustering accuracy. Require scikit-learn installed
+
+    # Arguments
+        y: true labels, numpy.array with shape `(n_samples,)`
+        y_pred: predicted labels, numpy.array with shape `(n_samples,)`
+
+    # Return
+        accuracy, in [0,1]
+    """
+    y_true = y_true.astype(np.int64)
+    D = max(y_pred.max(), y_true.max()) + 1
+    w = np.zeros((D, D), dtype=np.int64)
+    for i in range(y_pred.size):
+        w[y_pred[i], y_true[i]] += 1
+    row_ind, col_ind = linear_sum_assignment(w.max() - w)
+    return sum([w[i, j] for i, j in zip(row_ind, col_ind)]) * 1.0 / y_pred.size
+
+def nmi(y_true, y_pred):
+    return normalized_mutual_info_score(y_true, y_pred)
+
+def ari(y_true, y_pred):
+    return adjusted_rand_score(y_true, y_pred)
